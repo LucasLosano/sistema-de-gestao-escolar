90f24972bb6bab148864ad9fe86a15b034fafeb9
RafaelPignatari <rafaelpignatarifantinati@hotmail.com>
2022-05-15 00:32:46 -0300

    Correção de Exceptions (Interface -> Class)

--- ClassesLib/src/main/java/br/cefsa/edu/classeslib/exception/messages/FrequenciasException.java
+++ ClassesLib/src/main/java/br/cefsa/edu/classeslib/exception/messages/FrequenciasException.java
@@ -1,9 +1,9 @@
 package br.cefsa.edu.classeslib.exception.messages;
 
-public interface FrequenciasException {
+public final class FrequenciasException {
 
-    public final String MAIOR_QUE_ZERO = "É necessário pelo menos 1 frequência";
-    public final String MESMO_ALUNO = "Todas as frequências devem ser do mesmo aluno";
-    public final String MESMA_MATERIA = "Todas as frequências devem ser da mesma matéria";
-    public final String MESMO_PERIODO_LETIVO = "Todas as frequências devem ser do mesmo período Letivo";
+    public final static String MAIOR_QUE_ZERO = "É necessário pelo menos 1 frequência";
+    public final static String MESMO_ALUNO = "Todas as frequências devem ser do mesmo aluno";
+    public final static String MESMA_MATERIA = "Todas as frequências devem ser da mesma matéria";
+    public final static String MESMO_PERIODO_LETIVO = "Todas as frequências devem ser do mesmo período Letivo";
 }
--- ClassesLib/src/main/java/br/cefsa/edu/classeslib/exception/messages/PesosException.java
+++ ClassesLib/src/main/java/br/cefsa/edu/classeslib/exception/messages/PesosException.java
@@ -1,8 +1,8 @@
 package br.cefsa.edu.classeslib.exception.messages;
 
-public interface PesosException {
+public final class PesosException {
 
-    public final String IGUAL_A_QUATRO = "São necessários 4 pesos";
-    public final String DIFERENTE_DE_ZERO = "Os pesos devem ser diferentes de 0";
-    public final String SOMA_IGUAL_A_UM = "A soma dos pesos deve ser 1";
+    public final static String IGUAL_A_QUATRO = "São necessários 4 pesos";
+    public final static String DIFERENTE_DE_ZERO = "Os pesos devem ser diferentes de 0";
+    public final static String SOMA_IGUAL_A_UM = "A soma dos pesos deve ser 1";
 }
